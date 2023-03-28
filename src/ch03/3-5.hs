93be17889304e653e8f9627555acaa6955a529f7
libra <23025996+smirror@users.noreply.github.com>
2023-03-28 21:16:34 +0900

    add ch3-5

---
+++ src/ch03/3-5.hs
@@ -0,0 +1,18 @@
+head' :: [a] -> a
+head' [] = error "No head for empty lists!"
+head' (x:_) = x
+
+head' :: [a] -> a
+head' xs = case xs of [] -> "No head for empty lists!"
+                      (x:_) -> x
+
+describeList :: [a] -> String
+describeList ls = "The list is " ++ case ls of [] -> "empty."
+                                              [x] -> "a singleton list."
+                                              xs -> "a longer list."
+
+describeList :: [a] -> String
+describeList ls = "The list is " ++ what ls
+  where what [] = "empty."
+        what [x] = "a singleton list."
+        what xs = "a longer list."
